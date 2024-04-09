66e407b5981de61d0ad60e62203946cd7bbb077a
Biwash Budhathoki <075bct025@ioepc.edu.np>
2024-04-09 16:10:31 +0545

    Add react-router-dom package and update dependencies. Create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,29 @@
 import { useState } from 'react'
+import { BrowserRouter, Route, Routes} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Dashboard from './pages/Dashboard'
+import Projects from './pages/Projects'
+import Signin from './pages/Signin'
+import Signup from './pages/Signup'
+
+
 
 function App() {
   return (
     <>
-      <h1 className='text-3xl text-red-400'>Blog App</h1>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+
+
+      </Routes>
+      </BrowserRouter>
     </>
   )
 }
