ab2e4aac44da0c15153e8ae9ff2b04844f581308
Biwash Budhathoki <075bct025@ioepc.edu.np>
2024-04-16 14:24:18 +0545

    Add .env files to .gitignore and update package.json dependencies and make database connection

--- api/index.js
+++ api/index.js
@@ -1,5 +1,18 @@
-const express = require('express');
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
 const app = express();
+
+/* MONGOOSE SETUP */
+const MONGO_URL = process.env.MONGO_URI;
+
+console.log('MONGO_URI:', process.env.MONGO_URI);
+
+mongoose.connect(MONGO_URL)
+    .then(() => console.log('Connected to MongoDB'))
+    .catch((error) => console.log('Failed to connect to MongoDB:', error));
 
 app.listen(4000, () => {
     console.log('Server is running on port 4000');
