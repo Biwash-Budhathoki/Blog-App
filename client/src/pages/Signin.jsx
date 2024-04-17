1e291c32214ea0af756c2f68e9d1e4d141c59b56
Biwash Budhathoki <075bct025@ioepc.edu.np>
2024-04-17 22:05:29 +0545

    Update sign-in and sign-up pages with OAuth functionality

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -7,6 +7,7 @@ import {
   signInSuccess,
   signInFailure,
 } from '../redux/user/userSlice';
+import OAuth from '../components/OAuth';
 
 export default function SignIn() {
   const [formData, setFormData] = useState({});
@@ -80,7 +81,7 @@ export default function SignIn() {
               />
             </div>
             <Button
-            className=' text-white bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 w-[150px] mx-auto flex items-center justify-center '
+            className=' text-white bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 w-[362px] mx-auto flex items-center justify-center '
               gradientDuoTone='purpleToPink'
               type='submit'
               disabled={loading}
@@ -94,10 +95,11 @@ export default function SignIn() {
                 'Sign In'
               )}
             </Button>
+            <OAuth />
           </form>
           <div className='flex gap-2 text-sm mt-5'>
             <span>Don't have an account?</span>
-            <Link to='/sign-up' className='text-blue-500'>
+            <Link to='/signup' className='text-blue-500'>
               Sign Up
             </Link>
           </div>
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react'
 import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react';
 import { Link, useNavigate } from 'react-router-dom';
+import OAuth from '../components/OAuth';
 
 const Signup = () => {
   const [formData, setFormData] = useState({});
@@ -55,7 +56,7 @@ const Signup = () => {
       {/* right */}
 
       <div className='flex-2'>
-        <form className='flex flex-col gap-4 w-[300px]' onSubmit={handleSubmit}>
+        <form className='flex flex-col gap-4 w-[362px]' onSubmit={handleSubmit}>
           <div>
             <Label value='Your username' />
             <TextInput
@@ -84,7 +85,7 @@ const Signup = () => {
             />
           </div>
           <Button
-            className=' text-white bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 w-[150px] mx-auto flex items-center justify-center '
+            className=' text-white bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 w-[362px] mx-auto flex items-center justify-center '
             gradientDuoTone='purpleToPink'
             type='submit'
             disabled={loading}
@@ -98,11 +99,11 @@ const Signup = () => {
               'Sign Up'
             )}
           </Button>
-          {/* <OAuth /> */}
+          <OAuth />
         </form>
         <div className='flex gap-2 text-sm mt-5'>
           <span>Have an account?</span>
-          <Link to='/sign-in' className='text-blue-500'>
+          <Link to='/signin' className='text-blue-500'>
             Sign In
           </Link>
         </div>
