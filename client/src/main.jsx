5a5defa29e0932794516ae1e80365bf344ce2349
Biwash Budhathoki <075bct025@ioepc.edu.np>
2024-04-17 22:06:31 +0545

    Update Redux store configuration and add persistence

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,14 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
-import { store } from "./redux/store";
-import { Provider } from "react-redux";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { store, persistor } from './redux/store.js';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <PersistGate persistor={persistor}>
     <Provider store={store}>
-      <App />
+        <App />
     </Provider>
-  </React.StrictMode>
+  </PersistGate>
 );
