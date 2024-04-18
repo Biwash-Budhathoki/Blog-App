d0ad6e8f85448dddca7ac9e41e456b60d4738e63
Biwash Budhathoki <075bct025@ioepc.edu.np>
2024-04-18 16:22:26 +0545

    Refactor routing and add PrivateRoute component for authenticated routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import { BrowserRouter, Route, Routes} from 'react-router-dom'
-import Home from './pages/Home'
-import About from './pages/About'
-import Dashboard from './pages/Dashboard'
-import Projects from './pages/Projects'
-import Signin from './pages/Signin'
-import Signup from './pages/Signup'
-import Header from './components/Header'
-import FooterCom from './components/Footer'
-
-
+import { useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Dashboard from "./pages/Dashboard";
+import Projects from "./pages/Projects";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import Header from "./components/Header";
+import FooterCom from "./components/Footer";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
   return (
     <>
       <BrowserRouter>
-      <Header />
+        <Header />
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/signin" element={<Signin />} />
-        <Route path="/signup" element={<Signup />} />
-      </Routes>
-      
-      <FooterCom />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route element={<PrivateRoute />}>
+            <Route path="/dashboard" element={<Dashboard />} />
+          </Route>{" "}
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+
+        <FooterCom />
       </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
