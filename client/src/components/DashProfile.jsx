310b5c71333cd6fb0d299e6df8a0f626e13a5086
Biwash Budhathoki <075bct025@ioepc.edu.np>
2024-04-18 21:35:45 +0545

    Add DashProfile and DashSidebar components to implement dashboard layout

---
+++ client/src/components/DashProfile.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DashProfile = () => {
+  return (
+    <div>
+      This is content of Dashboard Profile
+    </div>
+  )
+}
+
+export default DashProfile
---
+++ client/src/components/DashSidebar.jsx
@@ -0,0 +1,41 @@
+import { Sidebar } from "flowbite-react";
+import { HiUser, HiArrowSmRight } from "react-icons/hi";
+import { useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+
+export default function DashSidebar() {
+  const location = useLocation();
+  const [tab, setTab] = useState("");
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get("tab");
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
+    }
+  }, [location.search]);
+
+  return (
+    <Sidebar className="w-full md:w-56">
+      <Sidebar.Items>
+        <Sidebar.ItemGroup className="flex flex-col gap-1">
+          <Link to="/dashboard?tab=profile">
+            <Sidebar.Item
+              className='hover:bg-teal-300 dark:text-black dark:hover:bg-[rgb(16,23,42)] dark:hover:text-white {tab === "profile" ? "bg-[rgb(16,23,42)] text-white" : ""}'
+              active={tab === "profile"}
+              icon={HiUser}
+              label="User"
+              labelColor="black"
+              as="div"
+            >
+              Profile
+            </Sidebar.Item>
+          </Link>
+
+          <Sidebar.Item icon={HiArrowSmRight} className="cursor-pointer ">
+            Sign Out
+          </Sidebar.Item>
+        </Sidebar.ItemGroup>
+      </Sidebar.Items>
+    </Sidebar>
+  );
+}
