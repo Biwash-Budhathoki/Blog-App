ff5b7fe7c677bc8d8205d643dd2aaf403f94483c
Biwash Budhathoki <075bct025@ioepc.edu.np>
2024-04-25 15:02:07 +0545

    add pre installation

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
 dotenv.config();
+import path from 'path';
 
 
 import userRoutes from './routes/user.route.js';
@@ -10,6 +11,7 @@ import authRoutes from './routes/auth.route.js';
 import postRoutes from './routes/post.route.js';
 import commentRoutes from './routes/comment.route.js';
 
+const __dirname = path.resolve();
 
 const app = express();
 app.use(express.json());
@@ -31,6 +33,12 @@ app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/post', postRoutes);
 app.use('/api/comment', commentRoutes);
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
 
 
 app.use((err, req, res, next) => {
